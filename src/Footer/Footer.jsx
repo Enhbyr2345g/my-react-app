065de0a6d89529a36eb5e16e72545eea85e9615c
Enhbyr2345g <menhbayar2538@gmail.com>
2025-05-28 18:53:38 +0800

    Create project

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer()
+{
+    const yeardate = new Date().getFullYear;
+    return (
+        <footer>
+            
+        </footer>
+    );
+}
+
+export default Footer
---
+++ src/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+function Header()
+{
+    return(
+        <header>
+            <h1> My WebSite</h1>
+
+            <ul>
+                <li>Menu</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    );
+}
+export default Header
